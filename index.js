72a9ecda9b7bff5a00fcbca4932455067ee18dc4
hjy080530 <bomaye119@gmail.com>
2026-04-28 01:18:11 +0900

    chore: read version from package.json and import runLogin

--- index.js
+++ index.js
@@ -5,30 +5,29 @@ const chalk = require('chalk');
 
 const config = require('./config');
 const { showSplash } = require('./splash');
-const { registerAuthCommands } = require('./commands/auth');
+const { registerAuthCommands, runLogin } = require('./commands/auth');
 const { registerNoteCommands } = require('./commands/note');
 const { registerDbCommands } = require('./commands/db');
 const { registerFeedCommands } = require('./commands/feed');
+const pkg = require('./package.json');
 
 program
   .name('obnofi')
   .description('Obnofi workspace CLI')
-  .version('1.0.0');
+  .version(pkg.version);
 
 registerAuthCommands(program);
 registerNoteCommands(program);
 registerDbCommands(program);
 registerFeedCommands(program);
 
-program.hook('preAction', (thisCommand, actionCommand) => {
+program.hook('preAction', async (thisCommand, actionCommand) => {
   const parentName = actionCommand.parent?.name();
-  const commandName = actionCommand.name();
-  if (parentName === 'auth' && ['login', 'logout'].includes(commandName)) return;
+  if (parentName === 'auth') return;
 
-  const token = config.getToken();
-  if (!token) {
-    console.error(chalk.red('✗'), '로그인이 필요해요. obnofi auth login');
-    process.exit(1);
+  if (!config.getToken()) {
+    console.log(chalk.dim('토큰이 없어서 로그인부터 진행합니다.'));
+    await runLogin();
   }
 });
 
@@ -42,5 +41,3 @@ if (process.argv.length === 2) {
 } else {
   program.parse(process.argv);
 }
-
-
